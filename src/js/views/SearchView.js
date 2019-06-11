6b5a0aee00c2a4d60181c3cc15cc6c22004cf0bf
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2019-06-11 20:22:01 +0100

    change arg in renderBook

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -3,13 +3,13 @@ import { elements } from './base';
 export const getInput = () => elements.searchInput.value;
 
 
-const renderBook = el => {
+const renderBook = book => {
 	const markup = `
 		<li>
-	        <a class="results__link" href="#${el.id}">
+	        <a class="results__link" href="#${book.id}">
 	            <div class="results__data">
-	                <h4 class="results__name">${el.volumeInfo.title}</h4>
-	                <p class="results__author">${el.volumeInfo.authors}</p>
+	                <h4 class="results__name">${book.volumeInfo.title}</h4>
+	                <p class="results__author">${book.volumeInfo.authors}</p>
 	            </div>
 	        </a>
 	    </li>   
