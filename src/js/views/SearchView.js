f74b9f38d423582124f6d17c808ee6b3f9fe54c0
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2019-06-11 21:23:41 +0100

    change css for left sidebar

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -6,10 +6,15 @@ export const getInput = () => elements.searchInput.value;
 const renderBook = book => {
 	const markup = `
 		<li>
-	        <a class="results__link" href="#${book.id}">
-	            <div class="results__data">
-	                <h4 class="results__name">${book.volumeInfo.title}</h4>
-	                <p class="results__author">${book.volumeInfo.authors}</p>
+	        <a class="results__link" href="#${book.id}">	        	
+	            <div class="results__data"> 
+		            <figure class="results__fig">
+		        		<img src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="${book.volumeInfo.title}">
+		        	</figure>
+		        	<div class="results__info">	
+		                <h4 class="results__name">${book.volumeInfo.title}</h4>
+		                <p class="results__author">${book.volumeInfo.authors}</p>
+	            	</div>
 	            </div>
 	        </a>
 	    </li>   
